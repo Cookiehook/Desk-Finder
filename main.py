531f747fcaa91fcfcbd105060b05bb5ee8fb0334
Cookiehook <shaun.harrison@live.co.uk>
2018-05-06 16:57:39 +0100

    Added method for calculating cost of a path Renamed "desk" to "node" to keep project generic.

--- main.py
+++ main.py
@@ -1,82 +1,97 @@
-from desk_objects import DeskObject
-import queue as Q
+from node_objects import NodeObject
+import queue as q
 import logging
 
 logging.basicConfig(level=logging.INFO)
 
-# Creating some nodes. Eventually moved into a database read command
-door = DeskObject("Door")
-a = DeskObject("Desk A")
-b = DeskObject("Desk B")
-c = DeskObject("Desk C")
-d = DeskObject("Desk D")
-e = DeskObject("Desk E")
-f = DeskObject("Desk F")
-g = DeskObject("Desk G")
-h = DeskObject("Desk H")
-i = DeskObject("Desk I")
-j = DeskObject("Desk J")
-
-# Adding node relations. Eventually moved into a database read command
-door.add_relationships((a, 4))
-a.add_relationships((door, 4), (b, 1), (g, 2))
-b.add_relationships((c, 1), (a, 1))
-c.add_relationships((b, 1))
-d.add_relationships((g, 5), (j, 1))
-e.add_relationships((g, 2), (f, 1), (h, 1))
-f.add_relationships((g, 15), (e, 1))
-g.add_relationships((a, 2), (d, 5), (e, 2), (f, 15))
-h.add_relationships((e, 1), (i, 2))
-i.add_relationships((h, 2), (j, 2))
-j.add_relationships((d, 1), (i, 3))
-
 
 def find_shortest_path(start, end):
 
-    visited_desks = []  # List of desks we've visited, used to check if we've reached the end
-    desk_path = []  # List of desks visited, and how we got there.
-    priority_queue = Q.PriorityQueue()
+    visited_nodes = []  # List of nodes we've visited, used to check if we've reached the end
+    node_path = []  # List of nodes visited, and how we got there.
+    priority_queue = q.PriorityQueue()
 
-    hack_counter = 0 #  Used to make sure the priority queue never tries to sort by a DeskObject. This needs to be implemented better in the future.
+    hack_counter = 0 #  Used to make sure the priority queue never tries to sort by a NodeOject. This needs to be implemented better in the future.
     priority_queue.put((0, hack_counter, start, start))
     hack_counter += 1
 
-    while end not in visited_desks:
+    while end not in visited_nodes:
         current_entry = priority_queue.get()
         current_cost = current_entry[0]
-        current_desk = current_entry[2]
-        previous_desk = current_entry[3]
+        current_node = current_entry[2]
+        previous_node = current_entry[3]
 
         # Loop through all nodes linked to the current one. Add them to the priority queue
         # if they haven't already been processed.
-        for destination, cost in current_desk.relationship_map.items():
-            if destination not in visited_desks:
-                priority_queue.put((current_cost + cost, hack_counter, destination, current_desk))
+        for destination, cost in current_node.relationship_map.items():
+            if destination not in visited_nodes:
+                priority_queue.put((current_cost + cost, hack_counter, destination, current_node))
                 logging.debug("Adding {0}, {1}, {2}, {3} to priority queue".format(
-                                current_cost + cost, hack_counter, destination.name, current_desk.name))
+                                current_cost + cost, hack_counter, destination.name, current_node.name))
                 hack_counter += 1
-        visited_desks.append(current_desk)
-        desk_path.append((current_desk, previous_desk))
+        visited_nodes.append(current_node)
+        node_path.append((current_node, previous_node))
 
-    for item in desk_path:
+    for item in node_path:
         logging.debug("{0} Processed by {1}".format(item[0].name, item[1].name))
 
-    desk_path.reverse()
+    node_path.reverse()
     shortest_route = []
     shortest_route.append(end)
 
-    current_desk = end
-    while current_desk is not start:
-        for entry in desk_path:
-            if entry[0] == current_desk:
+    # Back track through history to find the shortest path
+    current_node = end
+    while current_node is not start:
+        for entry in node_path:
+            if entry[0] == current_node:
                 shortest_route.append(entry[1])
-                current_desk = entry[1]
-                break # Prevents start from being added twice
-
+                current_node = entry[1]
+                break  # Prevents start from being added twice
     shortest_route.reverse()
     return shortest_route
 
 
-for node in find_shortest_path(f, d):
-    logging.info(node.name)
+def get_cost_of_route(route):
+    current_cost = 0
 
+    while len(route) > 1:
+        current_cost += route[0].get_cost_to_node(route[1])
+        route.remove(route[0])
+
+    return current_cost
+
+
+if __name__ == "__main__":
+
+    # Creating some nodes. Eventually moved into a database read command
+    door = NodeObject("Door")
+    a = NodeObject("Desk A")
+    b = NodeObject("Desk B")
+    c = NodeObject("Desk C")
+    d = NodeObject("Desk D")
+    e = NodeObject("Desk E")
+    f = NodeObject("Desk F")
+    g = NodeObject("Desk G")
+    h = NodeObject("Desk H")
+    i = NodeObject("Desk I")
+    j = NodeObject("Desk J")
+
+    # Adding node relations. Eventually moved into a database read command
+    door.add_relationships((a, 4))
+    a.add_relationships((door, 4), (b, 1), (g, 2))
+    b.add_relationships((c, 1), (a, 1))
+    c.add_relationships((b, 1))
+    d.add_relationships((g, 5), (j, 1))
+    e.add_relationships((g, 2), (f, 1), (h, 1))
+    f.add_relationships((g, 15), (e, 1))
+    g.add_relationships((a, 2), (d, 5), (e, 2), (f, 15))
+    h.add_relationships((e, 1), (i, 2))
+    i.add_relationships((h, 2), (j, 2))
+    j.add_relationships((d, 1), (i, 3))
+
+    path = find_shortest_path(door, i)
+    cost = get_cost_of_route(path)
+    for node in path:
+        logging.info(node.name)
+
+    logging.info("Total cost is: %s " % cost)
--- node_objects.py
+++ node_objects.py
@@ -1,4 +1,4 @@
-class DeskObject:
+class NodeObject:
 
     def __init__(self, name):
         self.name = name
@@ -7,3 +7,6 @@ class DeskObject:
     def add_relationships(self, *args):
         for arg in args:
             self.relationship_map[arg[0]] = arg[1]
+
+    def get_cost_to_node(self, node):
+        return self.relationship_map[node]
